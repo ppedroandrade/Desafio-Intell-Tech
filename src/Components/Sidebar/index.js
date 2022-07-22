a17f94edce3b55e2d3ff70220c2035fe1a34e3d4
Pedro Andrade <86939238+ppedroandrade@users.noreply.github.com>
2022-07-22 03:24:59 -0300

    mover a imagem

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import Draggable from "react-draggable";
+
+
 import * as AiIcons from "react-icons/ai";
+import * as GoIcons from "react-icons/go";
 
 import "./sidebar.css";
 
@@ -7,6 +11,18 @@ function Navbar() {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  const [img, setImg] = useState();
+
+  const onImageChange = (e) => {
+    const [file] = e.target.files;
+    setImg(URL.createObjectURL(file));
+  };
 
   return (
     <>
@@ -21,10 +37,34 @@ function Navbar() {
       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
         <div className="nav-menu-items" onClick={showSidebar}>
           <span className="navbar-toggle">
-            <AiIcons.AiOutlineClose size={40} color={'#f1f1f1'} onClick={showSidebar} />
+            <AiIcons.AiOutlineClose
+              size={40}
+              color={"#f1f1f1"}
+              onClick={showSidebar}
+            />
           </span>
         </div>
+
+        <div className="sideBarBtn">
+          <button className="sideBarBtn-gphc">
+            <GoIcons.GoGraph size={40} color={"black"} />
+            <span className="gphc-span">Gerar gráfico aleatorio</span>
+          </button>
+        </div>
+        <button className="sideBarBtn-img" onClick={handleClick}>
+          <AiIcons.AiOutlineFileImage size={70} color={"black"} />
+          <span className="img-span">Fazer upload de imagem</span>
+          <input
+            style={{ display: "none" }}
+            ref={inputRef}
+            type="file"
+            onChange={onImageChange}
+          />
+        </button>
       </nav>
+      <Draggable>
+        <img src={img} alt="" />
+      </Draggable>
     </>
   );
 }
